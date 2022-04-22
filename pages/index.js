7da87598d9c092b2c602058b48b2b70089d1148c
Michael Owuor <michael.owuor@gmail.com>
2022-04-22 14:58:36 +0300

    chore: using query in the static props.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,56 @@
-const Home = () => {
+import React, { useState } from 'react';
+import { gql } from '@apollo/client';
+import client from '../utilities/ApolloClient';
+import Meta from '../utilities/Meta';
+
+export default function Home(results) {
+	const initialState = results;
+	const [characters, setCharacters] = useState(initialState.characters);
+
+	console.log(initialState);
+
 	return (
-		<div>
-			<h1 className="px-2 text-center text-4xl font-medium text-indigo-500">
-				Hello? Next.js and Tailwind CSS have been correctly configured.
-			</h1>
+		<div className="">
+			<Meta title="Home | The Rick and Morty API | Superior Software Solutions" />
 		</div>
 	);
-};
+}
 
-export default Home;
+export const getStaticProps = async () => {
+	const { data } = await client.query({
+		query: gql`
+			query {
+				characters(page: 1) {
+					info {
+						count
+						pages
+					}
+					results {
+						id
+						name
+						location {
+							id
+							name
+						}
+						origin {
+							id
+							name
+						}
+						episode {
+							id
+							episode
+							air_date
+						}
+						image
+					}
+				}
+			}
+		`,
+	});
+
+	return {
+		props: {
+			characters: data.characters.results,
+		},
+	};
+};
--- utilities/ApolloClient.js
+++ utilities/ApolloClient.js
@@ -2,10 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 /** configure the apollo graphql client */
 const client = new ApolloClient({
-	uri:
-		process.env.NODE_ENV === 'production'
-			? 'https://rickandmortyapi.com/graphql/'
-			: 'https://rickandmortyapi.com/graphql/',
+	uri: 'https://rickandmortyapi.com/graphql/',
 	cache: new InMemoryCache(),
 });
 
